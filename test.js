1803bc7b0f41b8b0bf515b2638cf3263f6135d0f
Siddharth Kannan <kannan.siddharth12@gmail.com>
2015-07-04 15:25:17 +0530

    Semistandard format these files
    Signed-off-by: Siddharth Kannan <kannan.siddharth12@gmail.com>

--- test.js
+++ test.js
@@ -1,64 +1,65 @@
+/* global it, describe, beforeEach */
 'use strict';
 var path = require('path');
 var helpers = require('yeoman-generator').test;
 var assert = require('yeoman-assert');
 
 describe('generator', function () {
-	beforeEach(function (cb) {
-		var deps = ['../app'];
+  beforeEach(function (cb) {
+    var deps = ['../app'];
 
-		helpers.testDirectory(path.join(__dirname, 'temp'), function (err) {
-			if (err) {
-				cb(err);
-				return;
-			}
+    helpers.testDirectory(path.join(__dirname, 'temp'), function (err) {
+      if (err) {
+        cb(err);
+        return;
+      }
 
-			this.generator = helpers.createGenerator('nm:app', deps, null, {skipInstall: true});
-			cb();
-		}.bind(this));
-	});
+      this.generator = helpers.createGenerator('nm-semistandard:app', deps, null, {skipInstall: true});
+      cb();
+    }.bind(this));
+  });
 
-	it('generates expected files', function (cb) {
-		var expected = [
-			'.editorconfig',
-			'.gitattributes',
-			'.gitignore',
-			'.jshintrc',
-			'.travis.yml',
-			'index.js',
-			'license',
-			'package.json',
-			'readme.md',
-			'test.js'
-		];
+  it('generates expected files', function (cb) {
+    var expected = [
+      '.editorconfig',
+      '.gitattributes',
+      '.gitignore',
+      '.jshintrc',
+      '.travis.yml',
+      'index.js',
+      'license',
+      'package.json',
+      'readme.md',
+      'test.js'
+    ];
 
-		helpers.mockPrompt(this.generator, {
-			moduleName: 'test',
-			githubUsername: 'test',
-			website: 'test.com',
-			cli: false
-		});
+    helpers.mockPrompt(this.generator, {
+      moduleName: 'test',
+      githubUsername: 'test',
+      website: 'test.com',
+      cli: false
+    });
 
-		this.generator.run(function () {
-			assert.file(expected);
-			cb();
-		});
-	});
+    this.generator.run(function () {
+      assert.file(expected);
+      cb();
+    });
+  });
 
-	it('CLI option', function (cb) {
-		helpers.mockPrompt(this.generator, {
-			moduleName: 'test',
-			githubUsername: 'test',
-			website: 'test.com',
-			cli: true
-		});
+  it('CLI option', function (cb) {
+    helpers.mockPrompt(this.generator, {
+      moduleName: 'test',
+      githubUsername: 'test',
+      website: 'test.com',
+      cli: true
+    });
 
-		this.generator.run(function () {
-			assert.file('cli.js');
-			assert.fileContent('package.json', /"bin":/);
-			assert.fileContent('package.json', /"bin": "cli.js"/);
-			assert.fileContent('package.json', /"meow"/);
-			cb();
-		});
-	});
+    this.generator.run(function () {
+      assert.file('cli.js');
+      assert.fileContent('package.json', /"bin":/);
+      assert.fileContent('package.json', /"bin": "cli.js"/);
+      assert.fileContent('package.json', /"meow"/);
+      cb();
+    });
+  });
 });
